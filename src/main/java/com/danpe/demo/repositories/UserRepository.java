f8449bf15227835e3af5d60bab504950d8db2151
Daniel_PC <galvaomartinsdaniel@gmail.com>
2024-07-11 10:59:09 -0300

    Service layer, component registration

--- src/main/java/com/danpe/demo/repositories/UserRepository.java
+++ src/main/java/com/danpe/demo/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.danpe.demo.repositories;
 
 import com.danpe.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
